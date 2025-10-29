84869cab5398c0e886c9d04fc0fa25108c6b984c
Shahila Mafraz <fathima.shahila371@gmail.com>
2025-10-29 12:01:26 +0800

    Implemented Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
+import { Bot } from 'lucide-react';
 import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className="flex items-center justify-between max-w-5xl mx-auto">
+        {/* left side content */}
+        <div className='flex items-center space-x-3'>
+          <div className='p-2 bg-linear-to-r from bg-purple-500 to-indigo-600 rounded-full'>
+            <Bot className='h-6 w-6 text-white' />
+          </div>
+          <h1 className='text-xl font-bold'>Smart Chat</h1>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header
